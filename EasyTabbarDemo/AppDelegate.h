742aa39b31bf4dffac3445ba252389253a644e91
miyoo <hopk@163.com>
2018-02-08 10:16:50 +0800

    initial

---
+++ EasyTabbarDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  EasyTabbarDemo
+//
+//  Created by 腾盈 on 2018/2/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyTabbarDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  EasyTabbarDemo
+//
+//  Created by 腾盈 on 2018/2/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
